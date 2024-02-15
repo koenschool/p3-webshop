85559861b90b534818acb997e08326e1d26c6768
koen <9023005@student.zadkine.nl>
2024-02-15 08:36:03 +0100

    homepage en meer

---
+++ nav.php
@@ -0,0 +1,8 @@
+<nav class='menu'>
+<div class='menu2'>☰</div>
+<div class='menu-content'>
+    <a href='producten.php'>producten</a>
+    <a href='klanten.php'>klanten</a>
+    <a href='overons.php'>over ons</a>
+</div>
+</nav>
--- webshophome.php
+++ webshophome.php
@@ -3,17 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Strak in pak</title>
+    <script defer src="code.js"></script>
+    <link rel="stylesheet" href="style.css?<?php echo time(); ?>">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet">
 </head>
 <body>
-    <h1>test2</h1>
-    <h1>test2</h1>
-    <h1>test2</h1>
-    <h1>test2</h1>
-    <h1>test2</h1>
-    <h1>test2</h1>
-    <ul>
-
-    </ul>
+<?php include "nav.php" ?>
+     <header>
+        <h1>Welkom bij strak in pak.</h1>
+     </header>
+            <h2 class="left"><a href="producten.php">Strakke pakken.</a></h2>
+            <h2 class="right"><a href="klanten.php">Klanten.</a></h2><br><br><br><br>
+            <img class="left" src="img/pak2.jpg" alt="strak_pak" width="20%" height="20%">
+            <img  class="right" src="img/office.jpg" alt="mensen_in_kantoor" width="20%" height="20%">
+        <footer><a href="overons.php">over ons</a></footer>
 </body>
 </html>
